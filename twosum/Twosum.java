498f5b169a012116cea02fe848da16a0558122d8
Daniel H <daniel@haugboelle.dk>
2024-09-02 11:31:50 +0200

    delete test

--- twosum/Twosum.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Twosum {
-   public static void main(String[] args) {
-       Scanner scanner = new Scanner(System.in);
-       System.out.println(scanner.nextInt() + scanner.nextInt());
-       scanner.close();
-   }
-}
