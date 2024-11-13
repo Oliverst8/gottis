7944f5c97b734626e9d8d42b9a43262c5e6b598f
oliverst8 <oliver@starup99.dk>
2024-11-13 08:14:19 +0100

    Deleted twosum.java file

--- Twosum.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class Twosum{
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println(scanner.nextInt() + scanner.nextInt());
-        scanner.close();
-    }
-}
