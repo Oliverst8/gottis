bbae77466eb9f04d5044f9e5376457c82f5646fb
Oliver Starup <Oliver@starup99.dk>
2025-02-11 09:45:08 +0100

    Added flag parsing

--- cmd/gottis/gottis.go
+++ cmd/gottis/gottis.go
@@ -13,8 +13,21 @@ func Sanitize(text string) string {
 	return text
 }
 
+func parse() (map[string]string, string) {
+	flags := make(map[string]string)
+	choice := ""
+	for i, arg := range os.Args {
+		if strings.HasPrefix(arg, "-") {
+			flags[strings.TrimPrefix(arg, "-")] = os.Args[i+1]
+		} else if i != 0 && !strings.HasPrefix(os.Args[i-1], "-") {
+			choice = arg
+		}
+	}
+	return flags, choice
+}
+
 func main() {
-	_, err := internal.GetConfig()
+	config, err := internal.GetConfig()
 
 	// Setup gottis if it hasn't been setup before
 	if err != nil {
@@ -22,22 +35,32 @@ func main() {
 		return
 	}
 
-	if len(os.Args) < 2 {
+	flags, choice := parse()
+
+	if choice == "" {
 		log.Fatal("Please supply an argument when using gottis.\n\"gottis <argument>\" see \"gottis help\" for more info")
 	}
-	choice := Sanitize(os.Args[1])
+
+	choice = Sanitize(choice)
+	var language string
+	if flags["lang"] == "" {
+		language = config.DefaultLang
+	} else {
+		language = flags["lang"]
+	}
 
 	switch {
 	case choice == "i" || choice == "init":
 		if len(os.Args) != 3 {
 			log.Fatal("Please supply a name for the Kattis excercise when initializing. See \"gottis help\" for more info")
 		}
-		internal.Init(os.Args[2], "java")
+
+		internal.Init(os.Args[2], language)
 	case choice == "t" || choice == "test":
 
 		internal.Test()
 	case choice == "s" || choice == "submit":
-		
+
 		internal.Submit()
 	case choice == "setup":
 		internal.Setup()
